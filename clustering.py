a071569600a10ee1fffd580e94cf9615548514e4
rladmstj <pingdoll3110@naver.com>
2025-06-14 20:04:01 +0900

    clustering

---
+++ clustering.py
@@ -0,0 +1,7 @@
+from sklearn.cluster import KMeans
+import numpy as np
+
+X = np.array([[1, 2], [1, 4], [1, 0],
+              [10, 2], [10, 4], [10, 0]])
+kmeans = KMeans(n_clusters=2).fit(X)
+print(f"Cluster centers: {kmeans.cluster_centers_}")
